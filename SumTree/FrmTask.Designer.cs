45eeb04c36b0339b2d8bcb4e4bab47890e689db3
gedir <32949739+gedir@users.noreply.github.com>
2018-03-14 22:31:25 +0200

    Add files via upload

---
+++ SumTree/FrmTask.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SumTree
+{
+    partial class frmTask
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbExit = new System.Windows.Forms.Button();
+            this.rtTree = new System.Windows.Forms.RichTextBox();
+            this.cbRefresh = new System.Windows.Forms.Button();
+            this.tvTree = new System.Windows.Forms.TreeView();
+            this.rtBranches = new System.Windows.Forms.RichTextBox();
+            this.lRes = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbExit
+            // 
+            this.cbExit.Location = new System.Drawing.Point(823, 521);
+            this.cbExit.Name = "cbExit";
+            this.cbExit.Size = new System.Drawing.Size(75, 23);
+            this.cbExit.TabIndex = 0;
+            this.cbExit.Text = "Exit";
+            this.cbExit.UseVisualStyleBackColor = true;
+            this.cbExit.Click += new System.EventHandler(this.cbExit_Click);
+            // 
+            // rtTree
+            // 
+            this.rtTree.Location = new System.Drawing.Point(25, 33);
+            this.rtTree.Name = "rtTree";
+            this.rtTree.Size = new System.Drawing.Size(445, 220);
+            this.rtTree.TabIndex = 1;
+            this.rtTree.Text = "";
+            // 
+            // cbRefresh
+            // 
+            this.cbRefresh.Location = new System.Drawing.Point(451, 259);
+            this.cbRefresh.Name = "cbRefresh";
+            this.cbRefresh.Size = new System.Drawing.Size(75, 23);
+            this.cbRefresh.TabIndex = 2;
+            this.cbRefresh.Text = "Refresh";
+            this.cbRefresh.UseVisualStyleBackColor = true;
+            this.cbRefresh.Click += new System.EventHandler(this.cbRefresh_Click);
+            // 
+            // tvTree
+            // 
+            this.tvTree.Location = new System.Drawing.Point(492, 33);
+            this.tvTree.Name = "tvTree";
+            this.tvTree.Size = new System.Drawing.Size(406, 220);
+            this.tvTree.TabIndex = 4;
+            // 
+            // rtBranches
+            // 
+            this.rtBranches.Location = new System.Drawing.Point(25, 296);
+            this.rtBranches.Name = "rtBranches";
+            this.rtBranches.Size = new System.Drawing.Size(873, 175);
+            this.rtBranches.TabIndex = 5;
+            this.rtBranches.Text = "";
+            // 
+            // lRes
+            // 
+            this.lRes.AutoSize = true;
+            this.lRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lRes.Location = new System.Drawing.Point(22, 484);
+            this.lRes.Name = "lRes";
+            this.lRes.Size = new System.Drawing.Size(24, 20);
+            this.lRes.TabIndex = 6;
+            this.lRes.Text = "   ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(497, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Tree";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Full Branches";
+            // 
+            // frmTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 556);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lRes);
+            this.Controls.Add(this.rtBranches);
+            this.Controls.Add(this.tvTree);
+            this.Controls.Add(this.cbRefresh);
+            this.Controls.Add(this.rtTree);
+            this.Controls.Add(this.cbExit);
+            this.Name = "frmTask";
+            this.Text = "Pyramid";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cbExit;
+        private System.Windows.Forms.RichTextBox rtTree;
+        private System.Windows.Forms.Button cbRefresh;
+        private System.Windows.Forms.TreeView tvTree;
+        private System.Windows.Forms.RichTextBox rtBranches;
+        private System.Windows.Forms.Label lRes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SumTree/FrmTask.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SumTree
+{
+    public partial class frmTask : Form
+    {
+        string _goldenBranch = "";
+        Int32 _greatestSum = 0;
+        
+        public frmTask()
+        {
+            InitializeComponent();
+            RunTask();
+        }
+
+        private void RunTask()
+        {
+            // input from task pdf:
+            string sTree = @"215
+                            192 124
+                            117 269 442
+                            218 836 347 235
+                            320 805 522 417 345
+                            229 601 728 835 133 124
+                            248 202 277 433 207 263 257
+                            359 464 504 528 516 716 871 182
+                            461 441 426 656 863 560 380 171 923
+                            381 348 573 533 448 632 387 176 975 449
+                            223 711 445 645 245 543 931 532 937 541 444
+                            330 131 333 928 376 733 017 778 839 168 197 197
+                            131 171 522 137 217 224 291 413 528 520 227 229 928
+                            223 626 034 683 839 052 627 310 713 999 629 817 410 121
+                            924 622 911 233 325 139 721 218 253 223 107 233 230 124 233";
+            // visualisation of input:
+            string[] saTree = Regex.Split(sTree,"\r\n");
+            StringBuilder sbTree = new StringBuilder();
+            foreach (string sLine in saTree)
+            {
+                sbTree.AppendLine(sLine.Trim());
+            }
+            rtTree.Text = sbTree.ToString();
+
+            // Find branch with greatest sum:
+            Recalc();
+        }
+
+        private void cbExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cbRefresh_Click(object sender, EventArgs e)
+        {
+            Recalc();
+        }
+
+        private void AddChildren(TreeNode node,string[] saRows,int pnRow,int pnCol,string psBranch)
+        {
+            //node - current node looking for children
+            //saRows - string array with input values
+            //pnRow - row number of the current node
+            //pnCol - col number of the current node
+            //psBranch - branch as string of comma separated input values till the current node point (current node included)
+
+            //Add proper values from the next row to the current node as children
+
+            //current node value as string
+            string sNode = node.Text;
+
+            // taking next row
+            int nRow = pnRow + 1;
+            int nCol = pnCol;
+
+            int nVal = int.Parse(sNode);
+            string sNextRow = saRows[nRow];
+            // iterate through current (moving down from the current node) and next (moving diagonal from the current node) column
+            for (int i = 0; i < 2; i++)
+            {
+                string sBranch = psBranch;
+                nCol = nCol + i;
+                //taking input value of current or next column 
+                string sEl = (sNextRow.Split(' ')[nCol]).Trim();
+                int nElVal = int.Parse(sEl);
+
+                //checking if nVal (currenct node value) and nElVal (candidate node value) are different (one - even, another - odd)
+                if ((nVal + nElVal) % 2 == 1)
+                {
+                    // proper value, adding as child
+                    TreeNode nodeNew = node.Nodes.Add(nElVal.ToString());
+
+                    // extending branch with proper value
+                    sBranch = sBranch + "," + sEl;
+
+                    if (nRow < saRows.Length - 2)
+                    {
+                        // not the deepest level, looking further for childnodes
+                        AddChildren(nodeNew, saRows, nRow, nCol, sBranch);
+                    }
+                    else //deepest level reached
+                    {
+                        //sum branch values
+                        Int32 nBranchSum = SumBranch(sBranch);
+
+                        //logging full branch to visualise results
+                        rtBranches.Text = rtBranches.Text + sBranch + " (" + nBranchSum.ToString() + ")" + Environment.NewLine;
+
+                        if (nBranchSum > _greatestSum)
+                        {
+                            // this branch has greater sum as the last known. Assigning it to the "goldenBranch"
+                            _goldenBranch = sBranch;
+                            _greatestSum = nBranchSum;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Recalc()
+        {
+            //reset calculations (just richText rtTree is kept as input)
+            tvTree.Nodes.Clear();
+            _goldenBranch = "";
+            _greatestSum = 0;
+            rtBranches.Text = "";
+
+            //proceed with input
+            string[] saRows = Regex.Split(rtTree.Text, "\n");
+
+            // start with first line's value
+            string sNode = saRows[0].Trim();
+            TreeNode node = tvTree.Nodes.Add(sNode);
+
+            // add child nodes
+            AddChildren(node, saRows, 0, 0, sNode);
+
+            // show result
+            lRes.Text = "The branch with greatest sum (" + _greatestSum.ToString() + ") - " + _goldenBranch;
+        }
+
+        private Int32 SumBranch(string sBranch)
+        {
+            // sum branch values
+            Int32 nRet = 0;
+            string[] saVals = sBranch.Split(',');
+            foreach (string sVal in saVals)
+            {
+                nRet = nRet + Int32.Parse(sVal.Trim());
+            }
+            return nRet;
+        }
+
+    }
+}
